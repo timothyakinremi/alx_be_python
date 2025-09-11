8e9307c405bf3543c5dd6e2942afe8899d80c483
Timothy Akinremi <timothyakinremi@gmail.com>
2025-09-11 01:06:15 +0100

    Added simple_interest.py file

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
 principal = 1000 #principal
 rate = 0.05 #rate
 time = 3 #time
-interest = (principal*rate*time)
+interest = (principal * rate * time)
 
 print(f"The simple interest is: {interest}")
