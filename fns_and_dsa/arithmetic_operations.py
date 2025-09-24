a5dbbd2c8a434f48fdb7ce7b41c029203a006b25
Timothy Akinremi <timothyakinremi@gmail.com>
2025-09-24 21:13:58 +0100

    Added arithmetic_operations.py and main.py files

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1: float,num2: float,operation: str):
+def perform_operation(num1,num2,operation):
     # Perform a basic arithmetic operation between two numbers.
     if operation == "add":
         return num1 + num2
