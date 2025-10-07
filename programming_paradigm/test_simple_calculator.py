ffec35a4f8fa94448ad2d6ab14c5957209dbf641
timothyakinremi <93476992+timothyakinremi@users.noreply.github.com>
2025-10-07 13:08:41 +0100

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -23,26 +23,16 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(-5, 3), -8)
 
     # ---- Multiplication Tests ----
-    def test_multiply_positive_numbers(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(4, 3), 12)
-
-    def test_multiply_with_zero(self):
         self.assertEqual(self.calc.multiply(4, 0), 0)
-
-    def test_multiply_negative_numbers(self):
         self.assertEqual(self.calc.multiply(-4, -3), 12)
-
-    def test_multiply_mixed_numbers(self):
         self.assertEqual(self.calc.multiply(-4, 3), -12)
 
     # ---- Division Tests ----
-    def test_divide_positive_numbers(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
-
-    def test_divide_negative_numbers(self):
         self.assertEqual(self.calc.divide(-10, -2), 5)
-
-    def test_divide_mixed_numbers(self):
         self.assertEqual(self.calc.divide(-10, 2), -5)
 
     def test_divide_by_zero(self):
