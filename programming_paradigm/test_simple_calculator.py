3893be5d609bd12ba0aeef4a3908fe0fe0acf282
timothyakinremi <93476992+timothyakinremi@users.noreply.github.com>
2025-10-07 13:04:47 +0100

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,23 +11,15 @@ class TestSimpleCalculator(unittest.TestCase):
         self.calc = SimpleCalculator()
 
     # ---- Addition Tests ----
-    def test_add_positive_numbers(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
-
-    def test_add_negative_numbers(self):
         self.assertEqual(self.calc.add(-2, -3), -5)
-
-    def test_add_mixed_numbers(self):
         self.assertEqual(self.calc.add(-2, 3), 1)
 
     # ---- Subtraction Tests ----
-    def test_subtract_positive_numbers(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
-
-    def test_subtract_negative_numbers(self):
         self.assertEqual(self.calc.subtract(-5, -3), -2)
-
-    def test_subtract_mixed_numbers(self):
         self.assertEqual(self.calc.subtract(-5, 3), -8)
 
     # ---- Multiplication Tests ----
